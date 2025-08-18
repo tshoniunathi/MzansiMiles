40de7e61d22c3be35eeef1b0b477597a43defd94
Unathi Tshoni <tshoniunathi@gmail.com>
2025-08-19 00:56:22 +0200

    html restructure

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
-const menuBtn= document.getElementById("menu-btn");
-const navLinks= document.getElementById("nav-links");
-const menuBtnIcon= menuBtn.querySelector("i");
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
 
-menuBtn.addEventListener("click", (e)=> {
-    navLinks.classList.toggle("open");
+menuBtn.addEventListener("click", (e) => {
+  navLinks.classList.toggle("open");
 
-    const isOpen=navLinks.classList.contains("open");
-    menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line":"ri-menu-line");
+  const isOpen = navLinks.classList.contains("open");
+  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
 });
 
-navLinks.addEventListener("click", (e)=>{
-    navLinks.classList.remove("open");
-    menuBtnIcon.setAttribute("class", "ri-menu-line");
-})
+navLinks.addEventListener("click", (e) => {
+  navLinks.classList.remove("open");
+  menuBtnIcon.setAttribute("class", "ri-menu-line");
+});
 
+const scrollRevealOption = {
+  origin: "bottom",
+  distance: "50px",
+  duration: 1000,
+};
+
+scrollReveal().reveal(".header_img img", {
+  ...scrollRevealOption,
+  origin: "right",
+});
+scrollReveal().reveal(".header_content p", {
+  ...scrollRevealOption,
+  delay: 500,
+});
